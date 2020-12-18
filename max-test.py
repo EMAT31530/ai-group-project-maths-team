7db817d5e97c1d970cf6ea18317b82070b0e21e2
maxkirwan <73898539+maxkirwan@users.noreply.github.com>
2020-12-18 10:01:20 +0000

    Added group members

---
+++ max-test.py
@@ -0,0 +1,6 @@
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello, World!')
+Hello, World!
+>>> 
